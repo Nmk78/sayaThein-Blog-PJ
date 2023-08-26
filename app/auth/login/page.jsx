0513af17d6927ef589afe1175cad3903313ea883
Nmk78 <naymyokhant78@gmail.com>
2023-08-26 15:36:27 +0630

    Adding Google Auth

---
+++ app/auth/login/page.jsx
@@ -0,0 +1,12 @@
+import G_Auth from '@components/G_Auth'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+    <G_Auth />
+    </>
+  )
+}
+
+export default Login
--- components/Create.jsx
+++ components/Create.jsx
@@ -20,34 +20,42 @@ const modules = {
 };
 
 const formats = [
-  "header",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "indent",
-  "link",
-  "image",
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+  'color',
+
 ];
 
 const Create = () => {
   return (
     <div
-      id="editor"
-      className="w-full min-h-8/10 h-full flex flex-col justify-evenly bg-gray-300 dark:bg-slate-900"
+      id="Quill-editor"
+      className="w-full h-full flex flex-col justify-evenly bg-gray-300 dark:bg-slate-900"
     >
+
+      <div className="w-full h-1/2">
       <ReactQuill
         theme="snow"
         modules={modules}
         formats={formats}
-        className=" h-96  text-xl mx-1 text-gray-100 bg-gray-400 dark:bg-slate-900"
+        className=" h-full text-xl mx-1 text-gray-950 bg-gray-300 dark:text-gray-100 dark:bg-slate-900"
         // value={content}
         // onChange={setContent}
       />
-      <button className="rounded-lg bg-gray-400 text-gray-100 text-lg font-semibold dark:bg-cyan-700 px-4 py-1.5 w-fit my-10 mx-auto">
+      </div>
+
+      <button className="rounded-lg bg-gray-400 text-gray-100 text-lg font-semibold dark:bg-cyan-700 px-4 py-1.5 w-fit my-50 mx-auto">
         Post
       </button>
     </div>
